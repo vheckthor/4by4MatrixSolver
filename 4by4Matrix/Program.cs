758c6a5eb7e409e92bd258767be79750ddaf37c4
Dare Victor Adebayo <adebayovicktor@gmail.com>
2020-12-11 02:22:34 +0100

    first commit

---
+++ 4by4Matrix/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4by4Matrix
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var arr1 = new int[,] { 
+                     {3,0,2,6 },
+                     {2,4,0,3 },
+                     {5,2,1,1 },
+                     {7,6,3,4 }
+            };
+
+            var res = DeterminantVal(arr1);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+
+
+        static int DeterminantVal(int[,] input)
+        {
+             var arrlen = new List<List<int>>();
+
+            for(var x=0; x< 4; x++)
+            {
+                var create = new List<int>();
+                for (var y = 0; y < 4; y++)
+                    create.Add(input[x, y] );
+
+                arrlen.Add(create);
+            }
+
+            var listPut = arrlen;
+
+            var counter = listPut[0].Count;
+            var forward = 0;
+            var agg = new List<int>();
+
+            while (forward < counter)
+            {
+                var curr = new List<int>();
+                var copy = del.DeepClone(listPut);
+                var p = new del();
+                var ans =  p.deleter(copy, forward);
+
+                //convert back to matrix
+                var resolved = AnotherMethod(ans.Item1);
+
+                agg.Add(ans.Item2 * resolved);
+                forward++;
+            }
+
+            return agg.Sum();
+
+
+        }
+
+
+        public static int by3Method(List<List<int>> ans)
+        {
+            
+            var matrixedArray = del.ListtoArray(ans);
+            var by3resolver = del.by3Matrix(matrixedArray);
+
+            return by3resolver;
+        }
+
+
+        public static int AnotherMethod(List<List<int>> ans)
+        {
+            
+            var currentArray = new List<int>();
+            var ford = 0;
+            while (ford < ans.Count)
+            {
+                List<List<int>> sac = del.DeepClone(ans); //ans.Item1;
+                ;
+                var delete= new del();
+                var sol = delete.deleter(sac, ford);
+                var fin = sol.Item2 * ((sol.Item1[0][0] * sol.Item1[1][1]) - (sol.Item1[0][1] * sol.Item1[1][0]));
+                currentArray.Add(fin);
+                ford++;
+            }
+            return currentArray.Sum();
+        }
+
+
+    }
+
+
+
+    
+}
+
+
+
+
---
+++ 4by4Matrix/del.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace _4by4Matrix
+{
+    partial class Program
+    {
+        class del
+        {
+           public (List<List<int>>,int) deleter(List<List<int>> value, int index)
+           {
+                
+                var current = value[0][index];
+                if(index % 2 != 0)
+                {
+                    current = current * -1;
+                }
+                value.RemoveAt(0);
+                foreach(var x in value)
+                {
+                    x.RemoveAt(index);
+                }
+
+                return (value,current);
+           }
+
+
+
+
+            public static T DeepClone<T>(T obj)
+            {
+                using (var ms =new MemoryStream())
+                {
+                    var formatter = new BinaryFormatter();
+                    formatter.Serialize(ms, obj);
+                    ms.Position = 0;
+                    return (T)formatter.Deserialize(ms);
+                }
+            }
+
+
+            public static  int by3Matrix(int[,]val= default)
+            {
+                int i, j;
+                int[,] arr1 = val;
+                int det = 0;
+
+
+                //Console.Write("\n\nCalculate the determinant of a 3 x 3 matrix :\n");
+                //Console.Write("-------------------------------------------------\n");
+
+                //Console.Write("Input elements in the matrix :\n");
+                //for (i = 0; i < 3; i++)
+                //{
+                //    for (j = 0; j < 3; j++)
+                //    {
+                //        Console.Write("element - [{0}],[{1}] : ", i, j);
+                //        arr1[i, j] = Convert.ToInt32(Console.ReadLine());
+                //    }
+                //}
+                //Console.Write("The matrix is :\n");
+                //for (i = 0; i < 3; i++)
+                //{
+                //    for (j = 0; j < 3; j++)
+                //        Console.Write("{0}  ", arr1[i, j]);
+                //    Console.Write("\n");
+                //}
+
+                for (i = 0; i < 3; i++)
+                    det = det + (arr1[0, i] * (arr1[1, (i + 1) % 3] * arr1[2, (i + 2) % 3] - arr1[1, (i + 2) % 3] * arr1[2, (i + 1) % 3]));
+
+                Console.Write("\nThe Determinant of the matrix is: {0}\n\n", det);
+                return det;
+            }
+
+
+
+           public static int[,] ListtoArray(List<List<int>> array)
+            {
+                int i, j;
+                int[,] arr1 = new int[array.Count, array[0].Count];
+                for (i = 0; i < array.Count; i++)
+                {
+                    for (j = 0; j < array[0].Count; j++)
+                    {
+                        Console.Write("element - [{0}],[{1}] : ", i, j);
+                        arr1[i, j] = array[i][j];
+                    }
+                }
+
+                return arr1;
+            }
+
+
+        }
+
+
+    }
+
+
+
+    
+}
+
+
+
+
